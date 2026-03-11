d1f4cef017326106ae7281d19850ebd2808c9aba
Radu-A <elcastravete@gmail.com>
2026-03-11 12:39:38 +0100

    feat: setup routes and add /sync/user endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 
+import router from "./routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use("", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ src/routes.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { pool } from "./db";
+
+const router = Router();
+
+router.post(
+  "/sync/user",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { credential, email, name } = req.body;
+
+    try {
+      const query = `
+      INSERT INTO users (credential, email, name)
+      VALUES ($1, $2, $3)
+      ON CONFLICT (credential, email)
+      DO UPDATE SET name = EXCLUDED.name
+      RETURNING *;
+    `;
+      const values = [credential, email, name];
+      const result = await pool.query(query, values);
+
+      res.status(200).json({
+        message: "Successful synchronization",
+        user: result.rows[0],
+      });
+    } catch (error) {
+      // Delegamos el error al middleware global
+      next(error);
+    }
+  },
+);
+
+export default router;
